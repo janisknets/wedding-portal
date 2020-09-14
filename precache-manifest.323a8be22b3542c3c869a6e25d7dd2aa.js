8985c04411d4e4bedfab4a216e2d66aa20f7b23a
Janis Knets <janis.knets@accenture.com>
2020-09-15 02:33:33 +0300

    Updates

---
+++ precache-manifest.323a8be22b3542c3c869a6e25d7dd2aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236a9df32ec017dcd87ce2dabce1d93b",
+    "url": "/wedding/index.html"
+  },
+  {
+    "revision": "6fd53b1f5cc273f4e2dd",
+    "url": "/wedding/static/js/2.74544961.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/wedding/static/js/2.74544961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7fe5b1af3962502eb76",
+    "url": "/wedding/static/js/main.6a43d44d.chunk.js"
+  },
+  {
+    "revision": "8d3e3f34c14f9ad645c5",
+    "url": "/wedding/static/js/runtime-main.0bb40c67.js"
+  },
+  {
+    "revision": "0872760b636b92bea73d1e01853af9b1",
+    "url": "/wedding/static/media/butterfly.0872760b.svg"
+  },
+  {
+    "revision": "59ceab5aded9fee34e362028cf30dcc0",
+    "url": "/wedding/static/media/flowers.59ceab5a.svg"
+  }
+]);
